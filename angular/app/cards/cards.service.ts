03132183404798a0712618b3230289907371bf38
Shawn Carnegie <carnegieshawn@yahoo.ca>
2017-09-15 11:26:02 -0600

    Create addCardsForm in deck component
    Create addCardtoDeck and updateCard functions in cardsService
    new_cards_list is parsed to find both a quantity and cardName
     - if the card already exists in the deck the quantity is updated
     - if the card is not the deck the card is searched for in magicthegathering.io
        - if this returns a card the card is inserted into the deck

--- angular/app/cards/cards.service.ts
+++ angular/app/cards/cards.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 
 export class CardsService {
+  private token = localStorage.getItem('token');
+  private headers = new Headers({ 'Content-Type': 'application/json',
+                                  'Authorization': this.token });
+  private options = new RequestOptions({ headers: this.headers });
 
   constructor(private http: Http) {}
 
   getCard(cardName) {
-    return this.http.get(`https://api.magicthegathering.io/v1/cards?name="${cardName}"&contains=imageUrl`).map(res => res.json());
+    return this.http.get(`https://api.magicthegathering.io/v1/cards?name="${cardName}"&contains=imageUrl&pageSize=1`).map(
+      res => res.json());
+  }
+
+  addCardToDeck(deckId, card) {
+    return this.http.post(`http://localhost:3000/api/decks/${deckId}/card`, card, this.options).map(res => res.json());
+  }
+
+  updateCard(deckId, card) {
+    return this.http.put(`http://localhost:3000/api/decks/${deckId}/cards/${card._id}`, card, this.options);
   }
 }
--- angular/app/decks/deck/deck.component.ts
+++ angular/app/decks/deck/deck.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { toast } from 'angular2-materialize';
 
 import { AuthService } from '../../auth/auth.service';
 import { DeckService } from '../deck.service';
+import { CardsService } from '../../cards/cards.service';
 
 @Component({
   selector: 'app-deck',
@@ -14,11 +17,16 @@ export class DeckComponent implements OnInit {
   deck;
   ownedByCurrentUser = false;
   paramsSubscription;
+  addCardsForm: FormGroup;
+
+  new_card_list = new FormControl('', [Validators.required]);
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private authService: AuthService,
-    private deckService: DeckService
+    private cardsService: CardsService,
+    private deckService: DeckService,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit() {
@@ -27,6 +35,10 @@ export class DeckComponent implements OnInit {
         this.setDeckData(params['id']);
       }
     );
+
+    this.addCardsForm = this.formBuilder.group({
+      new_card_list: this.new_card_list
+    });
   }
 
   setDeckData(deckId) {
@@ -37,6 +49,72 @@ export class DeckComponent implements OnInit {
         if (this.deck.owner._id === this.authService.currentUser.id) {
           this.ownedByCurrentUser = true;
         }
+      }
+    );
+  }
+
+  addCardsSubmit() {
+    const newCardsArray = this.addCardsForm.value.new_card_list.split('\n');
+    for (const newCard of newCardsArray) {
+      const quantityAndName = newCard.split(/ (.+)/);
+      const quantityString = quantityAndName[0];
+      const cardName = quantityAndName[1];
+      const parsedQuantity = parseInt(quantityString, 10);
+
+      if (!parsedQuantity) {
+        toast(`${quantityString} is not a number`, 5000);
+        continue;
+      }
+
+      if (!cardName) {
+        toast(`no card name found next to ${quantityString}`, 5000);
+        continue;
+      }
+
+      const match = this.deck.cards.find((card) => {
+        return card.name.toUpperCase() === cardName.toUpperCase();
+      });
+
+      if (match) {
+        match.quantity += parsedQuantity;
+        this.cardsService.updateCard(this.deck._id, match).subscribe(
+          res => {
+            toast(`${match.name} quantity increased by ${parsedQuantity}`, 5000);
+          },
+          err => {
+            console.log(err);
+          }
+        );
+      } else {
+        this.cardsService.getCard(cardName).subscribe(
+          res => {
+            const fetchedCard = res.cards[0];
+            if (fetchedCard) {
+              const cardToSave = {
+                quantity: parsedQuantity,
+                name: fetchedCard.name,
+                imageUrl: fetchedCard.imageUrl
+              };
+              this.importCard(this.deck._id, cardToSave);
+            } else {
+              toast(`${cardName} could not be imported`, 5000);
+            }
+          },
+          err => {
+            console.log(err);
+          }
+        );
+      }
+    }
+  }
+
+  importCard(deckId, card) {
+    this.cardsService.addCardToDeck(deckId, card).subscribe(
+      res => {
+        this.deck.cards.push(card);
+      },
+      err => {
+        console.log(err);
       }
     );
   }
