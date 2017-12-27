2d99bd81554025e7fd4f444983474c3dee20ddde
Shawn Carnegie <carnegieshawn@yahoo.ca>
2017-12-26 17:47:27 -0700

    Set up production build to serve off localhost:3000
    Change angular to build to dist/public
    Install convurrently to run multiple commands in one line
     - Create dev command to run mongo, ng serve, and run nodemon off dist/server
     - Create prod command to run mongo, ng build, and run node off dist/server
    
    Change app.ts to use files in ../public
    Change app.ts to send all unmatched routes to ../public/index.html

--- server/app.ts
+++ server/app.ts
@@ -10,9 +10,9 @@ import setRoutes from './config/routes';
 const app = express();
 
 dotenv.load({ path: '.env' });
-
 app.set('port', process.env.PORT || 3000);
 
+app.use('/', express.static(path.join(__dirname, '../public')));
 app.use(cors());
 app.use(bodyParser.json({ limit: '1mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,6 +30,10 @@ mongoose.connection.on('error', (err) => {
 });
 
 setRoutes(app);
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 app.listen(app.get('port'), () => {
   console.log(`Server started on port ${app.get('port')}`);
